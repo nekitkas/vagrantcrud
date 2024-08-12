9e998472e6aad236152b2fe41560fccc49950984
Nikita Baranov <nekitkass@gmail.com>
2024-08-12 21:00:00 +0300

    script: add script for docker clean up

--- scripts/docker-delete-images.sh
+++ scripts/docker-delete-images.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Get a list of all running container IDs
-running_containers=$(docker ps -q)
+images=$(docker image ls -q)
 
 # Check if there are any running containers
-if [ -n "$running_containers" ]; then
+if [ -n "$images" ]; then
     # Stop all running containers
-    docker stop $running_containers
-    docker rm $running_containers
+    docker image rm $images
     echo "All running Docker containers have been stopped."
 else
     echo "No running Docker containers to stop."
